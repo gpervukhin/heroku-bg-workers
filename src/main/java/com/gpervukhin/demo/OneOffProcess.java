1db2d9a562148d109be858fd96cc67d0a2d509bb
Gennadiy Pervukhin <gpervukhin@gmail.com>
2018-06-22 19:19:00 +0300

    added argument to One-Off process

--- src/main/java/com/gpervukhin/demo/OneOffProcess.java
+++ src/main/java/com/gpervukhin/demo/OneOffProcess.java
@@ -4,5 +4,6 @@ public class OneOffProcess {
 	
 	public static void main(String[] args) {
 		System.out.println("%%%%% HELLO: OneOffProcess executed.");
+		System.out.println("%%%%% Arguments: " + (args.length > 0 ? args[0] : "no"));
 	}
 }
